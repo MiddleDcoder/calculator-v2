79186809342f9601a26d622d89f72b0506469b09
MiddleDcoder <33019593+MiddleDcoder@users.noreply.github.com>
2025-05-21 11:56:34 +0800

    Saving the setting into localStorage

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-const theme_toggler = document.querySelector(".light");
+const theme_toggler = document.querySelector(".theme");
 
 theme_toggler.addEventListener("click", () => {
   document.body.classList.toggle("dark_mode");
+  // Saving the setting
+  if (document.body.classList.contains("dark_mode")) {
+    localStorage.setItem("website_theme", "dark_mode");
+  } else {
+    localStorage.setItem("website_theme", "default");
+  }
 });
