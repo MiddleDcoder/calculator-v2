f033dcf8b45de7ec9b492b9d43f48f56879b74da
MiddleDcoder <33019593+MiddleDcoder@users.noreply.github.com>
2025-05-28 23:53:52 +0800

    New Approach: Calculator v2
    state: as object to handle the main data and display
    declared DOM variables
    operations object for simple math

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 /*  Main Calculator App */
+const state = {
+    firstOperand: null,
+    secondOperand: null,
+    operator: null,
+    display() {
+        return `${this.firstOperand || ''} ${this.operator || ''} ${this.secondOperand || ''}`.trim();
+    },
+}
+
+const display = document.querySelector(".display");
+const keys = document.querySelector(".keys");
+const allClear = document.querySelector(".all-clear");
+const back = document.querySelector(".back");
+const decimal = document.querySelector(".decimal");
+const sign = document.querySelector(".sign");
+const percentage = document.querySelector(".percentage");
+
+// Math operations
+const operations = {
+  "+": (a, b) => a + b,
+  "-": (a, b) => a - b,
+  "x": (a, b) => a * b,
+  "÷": (a, b) => a / b,
+};
